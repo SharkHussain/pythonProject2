10565b6f421330d903371f59521a38b6e2d3b9c8
Shakir <shakir.hussain@gmail.com>
2021-10-05 19:50:47 +0100

    branch 2

--- source/read_file.py
+++ source/read_file.py
@@ -4,22 +4,3 @@ pd.read_csv("file path")
 
 import pandas as pd
 
-pd.read_csv("file path")
-
-import pandas as pd
-
-pd.read_csv("file path")
-
-import pandas as pd
-
-pd.read_csv("file path")
-
-pd.read_csv("file path")
-
-import pandas as pd
-
-pd.read_csv("file path")
-
-import pandas as pd
-
-pd.read_csv("file path")
